3e798915b995fc213878102857da1a7fff35b9b6
Yannick Beauchamp-Hurtubise <ybeauchamp@nomadis.co>
2023-04-15 01:27:26 -0400

    Remove VANILLA_COST_OVERRIDE dialog for multiplayer

--- src/newhorizon/NHRegister.java
+++ src/newhorizon/NHRegister.java
@@ -14,60 +14,55 @@ import newhorizon.util.graphic.EffectDrawer;
 
 public class NHRegister{
 	public static final Seq<Runnable> afterLoad = new Seq<>();
-	
+
 	protected static boolean worldLoaded = false;
-	
+
 	public static void postAfterLoad(Runnable runnable){
 		if(!worldLoaded)afterLoad.add(runnable);
 	}
-	
+
 	static{
 		Net.registerPacket(LongInfoMessageCallPacket::new);
 	}
-	
+
 	private static void registerJsonClasses(){
-	
+
 	}
-	
+
 	public static boolean worldLoaded(){
 		return worldLoaded;
 	}
-	
+
 	public static void load(){
 		Events.on(EventType.ResetEvent.class, e -> {
 			NewHorizon.debugLog("Reset Event Triggered");
-			
+
 			NHGroups.clear();
 			worldLoaded = false;
 			afterLoad.clear();
 			EventHandler.dispose();
 		});
-		
+
 		Events.run(EventType.Trigger.draw, () -> {
 			NHModCore.core.renderer.draw();
 		});
-		
+
 //		Events.run(EventType.Trigger.postDraw, () -> {
 //			NHModCore.core.renderer.draw();
 //		});
-		
+
 		Events.on(EventType.WorldLoadEvent.class, e -> {
 			NewHorizon.debugLog("WorldLoad Event Triggered");
-			
+
 			NHGroups.resize();
 			NHModCore.core.initOnLoadWorld();
 			if(!Vars.state.isEditor()){
 				EventHandler.create();
 				afterLoad.each(Runnable::run);
 			}
-			
+
 			afterLoad.clear();
-			
-			if(Vars.net.active() && !NHSetting.getBool(NHSetting.VANILLA_COST_OVERRIDE)){
-				Vars.ui.showConfirm("@mod.ui.requite.need-override", NHSetting::showDialog);
-				Vars.player.con.close();
-			}
-			
+
 			Core.app.post(() -> {
 				if(Vars.state.isPlaying()){
 					Vars.state.rules.objectives.all.insert(0, new WorldEventObjective());
@@ -76,18 +71,18 @@ public class NHRegister{
 					worldLoaded = true
 				)));
 			});
-			
+
 			if(!Vars.headless){
 				EffectDrawer.drawer.clear();
 			}
 		});
-		
+
 		if(!Vars.headless)Events.on(EventType.StateChangeEvent.class, e -> {
 			if(e.to == GameState.State.menu){
 				worldLoaded = false;
 			}
 		});
 	}
-	
-	
+
+
 }
